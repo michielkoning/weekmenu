5b2491cc2faa69057be9655e3d40313b5c65835a
Michiel Koning <michiel.koning@valtech.com>
2021-09-21 14:01:32 +0200

    process.env.VUE_APP_MESSAGING_SENDER_ID

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   databaseURL: process.env.VUE_APP_DATABASE_URL,
   projectId: process.env.VUE_APP_PROJECT_ID,
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
