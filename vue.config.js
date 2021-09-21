81f36b418246abdc687e4c2ebf8cb4c49478dc6d
Michiel Koning <michiel.koning@valtech.com>
2021-09-21 14:58:04 +0200

    pwa

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const title = "Weekmenu";
+const color = "#d4e4a2";
+
+module.exports = {
+  pwa: {
+    name: title,
+    themeColor: color,
+    msTileColor: color,
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black-translucent",
+    manifestOptions: {
+      background_color: color,
+      theme_color: color,
+      categories: ["politics"],
+      lang: "nl",
+      name: title,
+      orientation: "portrait-primary",
+      short_name: title,
+    },
+  },
+};
