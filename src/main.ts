2d0171be6523a0e7c78b5795e4afd5e7e4f07a64
MIchiel Koning <michiel.koning@valtech.com>
2022-10-31 15:48:56 +0100

    remove sw

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./registerServiceWorker";
 import i18n from "./i18n";
 
 import "./assets/css/app.css";
