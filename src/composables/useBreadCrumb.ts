6b895a97451f30e6ed6b0731f456430d1f9e6303
MIchiel Koning <michiel.koning@valtech.com>
2022-11-11 14:46:02 +0100

    types

--- src/composables/useBreadCrumb.ts
+++ src/composables/useBreadCrumb.ts
@@ -1,4 +1,4 @@
-import type { IRecipeItem } from "@/types/IRecipe";
+import type { IRecipeItem } from "@/types/IBreadCrumb";
 import { ref, type Ref } from "vue";
 import { useRoute } from "vue-router";
 import type { RouteLocationNamedRaw } from "vue-router";
