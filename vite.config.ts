de140468e5c5df88523b4662204ece6a90def9cb
MIchiel Koning <michiel.koning@valtech.com>
2023-06-12 16:55:51 +0200

    packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,40 +10,40 @@ export default defineConfig({
     vue(),
     VitePWA({
       includeAssets: [
-        "/img/icons/favicon.ico",
-        "/img/icons/apple-touch-icon.png",
-        "/img/icons/masked-icon.svg",
+        '/img/icons/favicon.ico',
+        '/img/icons/apple-touch-icon.png',
+        '/img/icons/masked-icon.svg'
       ],
 
       manifest: {
-        name: "Weekmenu",
-        short_name: "Weekmenu",
-        description: "Weekmenu",
-        theme_color: "#121212",
+        name: 'Weekmenu',
+        short_name: 'Weekmenu',
+        description: 'Weekmenu',
+        theme_color: '#121212',
         icons: [
           {
-            src: "/img/icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: '/img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: "/img/icons/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
+            src: '/img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
         ],
         protocol_handlers: [
           {
-            protocol: "web+weekmenu",
-            url: "/%s",
-          },
-        ],
-      },
-    }),
+            protocol: 'web+weekmenu',
+            url: '/%s'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
